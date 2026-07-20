0888724be6212c69df5abd38c574807468d9ca8c
sakimuzzaman <sakimuzzaman104@gmail.com>
2026-07-21 01:39:09 +0600

    configure app entry, server and middlewares

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,6 +7,7 @@ import config from "../config/index.js";
 
 
 const auth = (...roles: string[]) => {
+
   return catchAsync(async (req: any, res: any, next: any) => {
     const token = req.headers.authorization?.split(" ")[1];
 
@@ -14,13 +15,16 @@ const auth = (...roles: string[]) => {
       throw new AppError(httpStatus.UNAUTHORIZED, "Unauthorized");
     }
 
+
     const decoded = jwt.verify(token, config.jwt.secret) as any;
+
 
     if (roles.length && !roles.includes(decoded.role)) {
       throw new AppError(httpStatus.FORBIDDEN, "Forbidden");
     }
 
     req.user = decoded;
+    
     next();
   });
 };
