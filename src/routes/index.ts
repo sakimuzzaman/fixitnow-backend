03ee7585787f7d3bcd1bf1381cb4767c4c4b6c3d
sakimuzzaman <sakimuzzaman104@gmail.com>
2026-07-22 19:15:08 +0600

    Update the cetralized routes with BookingRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { AuthRoutes } from '../modules/auth/auth.route.js';
 import { CategoryRoutes } from '../modules/category/category.route.js';
 import { ServiceRoutes } from '../modules/service/service.route.js';
 import { TechnicianRoutes } from '../modules/technician/technician.route.js';
+import { BookingRoutes } from '../modules/booking/booking.route.js';
 
 const router = express.Router();
 
@@ -26,6 +27,9 @@ const moduleRoutes = [
   {
     path: '/technician',
     route: TechnicianRoutes,
+  },
+  { path: '/bookings', 
+    route: BookingRoutes 
   },
 ];
 
