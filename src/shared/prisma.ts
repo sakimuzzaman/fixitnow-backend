9c8cc02d2e91c2c02dd0c27b4e045a95ba318dba
sakimuzzaman <sakimuzzaman104@gmail.com>
2026-07-28 05:00:50 +0600

    Fix Prisma 7 configuration

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -7,12 +7,30 @@
 // export default prisma;
 
 
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+
+// const prisma = new PrismaClient({
+//   adapter,
+//   log: ["error", "warn"],
+// });
+
+// export default prisma;
+
+
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
+
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
   adapter,
