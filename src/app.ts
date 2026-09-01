b2391a6d7abf76320898d0b029db3d53266ba4db
sakimuzzaman <sakimuzzaman104@gmail.com>
2026-09-01 19:13:09 +0600

    update the app.ts file cors method

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,11 @@ import router from './routes/index.js';
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('FixItNow API is running...'));
